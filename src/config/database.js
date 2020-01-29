51a915801da821f9c601a6deb78bf65bc3aebacf
Dhemes Mota <dhemes.mota@gmail.com>
2020-01-29 02:37:40 -0300

    Adicionando migration de usuário

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: '123456',
+  password: 123456,
   database: 'fastfeet',
   define: {
     timestamps: true,

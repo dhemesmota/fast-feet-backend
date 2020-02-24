30d2c751faa5c7b072a1041b0dd285807393f118
Dhemes Mota <dhemes.mota@gmail.com>
2020-02-23 22:31:17 -0300

    Validação de dados

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -35,7 +35,15 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
-      password: Yup.string().min(6),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
     });
 
     if (!(await schema.isValid(req.body))) {
